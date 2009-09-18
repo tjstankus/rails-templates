f3c9998502ab69aefaac7eda7e8d0bc98b374e24
TJ Stankus <tj@stank.us>
2009-09-18 17:56:06 -0400

    A bit o' cleanup.

--- base_template.rb
+++ base_template.rb
@@ -1,11 +1,26 @@
 # TODO: Create function in bash profile similar to Railscast 148
 
+# General file cleanup
 run "echo TODO > README"
 run "mv public/index.html public/index.orig.html"
 run "rm -f public/javascripts/*"
+run "cp config/database.yml config/database_example.yml"
 
+# YUI reset.css
+run "curl -L http://yui.yahooapis.com/2.8.0r4/build/reset/reset-min.css > public/stylesheets/reset-min.css"
+
+# jQuery
+if yes?("Do you want to use jQuery?")
+  jquery_version = ask("What version of jQuery? (Just enter for 1.3.2)")
+  jquery_filename = jquery_version.empty? ? "jquery-1.3.2.min.js" : "jquery-#{jquery_version}.min.js"
+  jquery_dest = "public/javascripts/#{jquery_filename}"
+  run "curl -L http://jqueryjs.googlecode.com/files/#{jquery_filename} > #{jquery_dest}"
+end
+
+# Application layout with HAML
+# TODO: Get jQuery in there if it's being used.
 file "app/views/layouts/application.html.haml", <<-END
-!!! Strict
+!!!
 %html{html_attrs}
   
   %head
@@ -24,17 +39,9 @@ file "app/views/layouts/application.html.haml", <<-END
         %h1=h yield(:title)
       
       = yield
-
 END
 
-if yes?("Do you want to use jQuery?")
-  version = ask("What version of jQuery? (Just enter for 1.3.2)")
-  jquery_version = version.empty? ? "1.3.2" : version
-  run "curl -L http://jqueryjs.googlecode.com/files/jquery-#{jquery_version}.min.js > public/javascripts/jquery-#{jquery_version}.min.js"
-end
-
-run "curl -L http://yui.yahooapis.com/2.8.0r4/build/reset/reset-min.css > public/stylesheets/reset-min.css"
-
+# .gitignore
 file ".gitignore", <<-END
 .DS_Store
 log/*.log
@@ -42,13 +49,10 @@ tmp/**/*
 config/database.yml
 db/*.sqlite3
 END
-
 run "touch tmp/.gitignore log/.gitignore vendor/.gitignore"
-run "cp config/database.yml config/database_example.yml"
 
+# Gems
 gem('haml')
-
-# Testing tools
 gem('rspec', :lib => false, :env => 'test')
 gem('rspec-rails', :lib => false, :env => 'test')
 gem('thoughtbot-factory_girl', :lib => 'factory_girl', :source => 'http://gems.github.com', :env => 'test')
@@ -57,9 +61,11 @@ gem('webrat', :env => 'test')
 
 # rake('gems:install', :sudo => true)
 
+# Generators
 generate :rspec
 generate :cucumber
 
+# Git'r done
 git :init
 git :add => '.'
 git :commit => "-m 'Initial commit.'"
